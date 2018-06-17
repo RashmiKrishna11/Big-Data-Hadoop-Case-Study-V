8ba6ec62eb0427f8016a59dd866797b08c32e93d
RashmiKrishna11 <36598558+RashmiKrishna11@users.noreply.github.com>
2018-06-17 17:25:34 +0530

    Case Study V - Case study on Spark Streaming
    There are two parts this case study
    ⁃ First Part​ - You have to create a Spark Application which streams data
    from a file on local directory on your machine and does the word count
    on the fly. The word should be done by the spark application in such a
    way that as soon as you drop the file in your local directory, your spark
    application should immediately do the word count for you.
    
    ⁃ Second Part - ​In this part, you will have to create a Spark Application
    
    which should do the following
    
    1. Pick up a file from the local directory and do the word count
    2. Then in the same Spark Application, write the code to put the
    
    same file on HDFS.
    
    3. Then in same Spark Application, do the word count of the file
    
    copied on HDFS in step 2
    
    4. Lastly, compare the word count of step 1 and 2. Both should
    
    match, other throw an error

--- Wordcount.scala
+++ Wordcount.scala
@@ -1,36 +1,17 @@
-package com.acadgild.spark
+package SparkstreamingAssignments
 
-import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
- 
+import org.apache.spark.streaming._
 object Wordcount {
- def main(args: Array[String]) {
-  
- //Create conf object
- val conf = new SparkConf().setMaster("local[*]")
- .setAppName("WordCount")
-  
- //create spark context object
- val sc = new SparkContext(conf)
- 
-//Check whether sufficient params are supplied
- if (args.length < 1) {
- println("Usage: ScalaWordCount <input> <output>")
- System.exit(1)
- }
- //Read file and create RDD
- val rawData = sc.textFile("/home/acadgild/wordcount")
-  
- //convert the lines into words using flatMap operation
- val words = rawData.flatMap(line => line.split(" "))
-  
- //count the individual words using map and reduceByKey operation
- val wordCount = words.map(word => (word, 1)).reduceByKey(_ + _)
-  
- //Save the result
- wordCount.saveAsTextFile(args(0))
- 
-//stop the spark context
- sc.stop
- }
+
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("TextFileStream").setMaster("local[*]")
+    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    val lines = ssc.textFileStream("e:\\sparkstreaming")
+    val wordcounts = lines.flatMap(line => line.split(" ").map(word =>(word,1))).reduceByKey(_ + _)
+    wordcounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
 }
